4ae6f67fb1e111725dbd65cff5409db569df4bea
kevinbargary <kevinbargary@gmail.com>
2014-03-09 17:24:44 +0000

    MongoDB, Node JS, Jade Template

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' });
+};
+
+exports.helloworld = function(req, res){
+  res.render('helloworld', { title: 'Hello World!' });
+};
+
+exports.userlist = function (db) {
+    return function (req, res) {
+        var collection = db.get('usercollection');
+        collection.find({}, {}, function (e, docs) {
+            res.render('userlist', { title: 'User List', 'userlist': docs });
+        });
+    };
+};
